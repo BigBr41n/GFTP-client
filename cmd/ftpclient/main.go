5e56fed79cf0b45fb9c0cccf2c3bfe85c7a3c300
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-09-28 15:40:01 +0200

    initiate a TCP connection to a server and print the response

--- cmd/ftpclient/main.go
+++ cmd/ftpclient/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+
+
+func main() {
+	//parse arguments passed
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: gftp <host> <port>")
+        return
+	}
+
+	fmt.Println(os.Args[1], os.Args[2])
+	host := fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]);
+
+	//connect to FTP server
+	conn , err := net.Dial("tcp", host);
+	if err!= nil {
+        fmt.Printf("Error connecting to FTP server: %v\n", err)
+        return
+    }
+
+	//read server response
+	buf := make([]byte, 1024)
+    conn.Read(buf)
+    fmt.Println(string(buf))
+
+    //close connection
+    conn.Close()
+}
